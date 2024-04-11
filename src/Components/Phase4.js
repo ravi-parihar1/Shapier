7a5a8511cfda6395ff419367c5c8db7eefe97d14
ravi-parihar1 <91816794+ravi-parihar1@users.noreply.github.com>
2024-04-11 14:42:34 +0530

    icons addon

--- src/Components/Phase4.js
+++ src/Components/Phase4.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Lottie from 'react-lottie';
+import { PiWhatsappLogoLight } from "react-icons/pi";
+import { LuPhoneCall } from "react-icons/lu";
 import animatefile from '../assets/HomePageAssets/Animation .json';
 import ani from '../assets/HomePageAssets/Animation2.json'
 import './Phase4.css'
@@ -44,8 +46,8 @@ export default function Phase4() {
                     <h1>Request a Quote</h1>
                     <p>order in bulk Easily with our special <br />chat and call options</p>
                     <div className="but">
-                        <button className="whatsapp-button mt-4">WhatsApp</button>
-                        <button className="call-button mt-4">Call</button>
+                        <button className="whatsapp-button mt-4 "><PiWhatsappLogoLight className="icon"/>WhatsApp</button>
+                        <button className="call-button mt-4"><LuPhoneCall className="icon"/>Call</button>
                     </div>
                 </div>
                 <div className="ani-container">

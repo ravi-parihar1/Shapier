be08fb98bef38535da659b9b69d583884fcd0b55
ravi-parihar1 <91816794+ravi-parihar1@users.noreply.github.com>
2024-04-12 08:50:15 +0530

    Responsive till phase2 tablte

--- src/Components/ServiceCard.js
+++ src/Components/ServiceCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ServiceCard({ imageSrc, title }) {
     return (
-        <div className='container-card px-1s'>
+        <div className='container-card'>
             <img src={imageSrc} alt={title} />
             <h6 className="service-title ">{title}</h6>
         </div>

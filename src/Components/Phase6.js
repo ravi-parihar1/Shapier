0323b4fa77b3596155f5797995fdd7c7daf2dc23
ravi-parihar1 <91816794+ravi-parihar1@users.noreply.github.com>
2024-04-16 23:29:34 +0530

    catergories update

--- src/Components/Phase6.js
+++ src/Components/Phase6.js
@@ -1,32 +1,57 @@
-import React from 'react'
-import '../css/Phase6.css'
-import fisrt from '../assets/HomePageAssets/1.svg'
-import sec from '../assets/HomePageAssets/2.svg'
-import third from '../assets/HomePageAssets/3.svg'
-
+import { React, useState, useEffect } from 'react';
+import Services from '../Services.json';
+import Shop from '../TopRatedProduct.json';
+import Shap from '../Shap.json'
+import TopProduct from '../TopRatedProduct.json';
+import '../css/Phase6.css';
 
 export default function Phase6() {
-  const buttonsData = [
-    { title: "Service", imageSrc: fisrt },
-    { title: "Shape", imageSrc: sec },
-    { title: "Store", imageSrc: third },
-  ];
+  const tags = ["services", "shap", "shop"];
+  const [displayedProducts, setDisplayedProducts] = useState("services")
 
+  const handleClick = async (tag) => {
+    setDisplayedProducts(tag);
+  }
+
+  useEffect(() => {
+    console.log(displayedProducts);
+  }, [displayedProducts]);
 
   return (
-    <div className="phase6-container">
-      {buttonsData.map((button, index) => (
-        <ServiceButton key={index} title={button.title} imageSrc={button.imageSrc} />
-      ))}
-    </div>
+    <>
+      <div className='tags_container'>
+        {tags.map(tag =>
+          <div className='tags_inner_container'>
+            <button className='gm-btn gb-cutter' key={tag} onClick={() => handleClick(tag)}>{tag}</button>
+          </div>
+        )}
+      </div>
+      {displayedProducts === "services" ?
+        <div className='cat-item'>
+          {Services.map(service =>
+            <div className='tags_element' key={service.id}>
+              <img src={service.img} alt={service.name} />
+              {/* <p>{service.name}</p> */}
+            </div>
+          )}
+        </div> : null}
+      {displayedProducts === "shap" ?
+        <div className='cat-item'>
+          {Shap.map(shap =>
+            <div className='tags_element' key={shap.id}>
+
+              {shap.name}
+            </div>
+          )}
+        </div> : null}
+      {displayedProducts === "shop" ?
+        <div className='cat-item'>
+          {Shop.map(shop =>
+            <div className='tags_element' key={shop.id}>
+              {shop.name}
+            </div>
+          )}
+        </div> : null}
+    </>
   )
-}
-
-function ServiceButton({ title, imageSrc }) {
-  return (
-    <div className="service-button">
-      <img src={imageSrc} alt={title} />
-      <span className='cat-name'>{title}</span>
-    </div>
-  );
 }

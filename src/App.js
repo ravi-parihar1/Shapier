bf876d72c2175bdd026143a6bed52d2ebaa585e9
ravi-parihar1 <91816794+ravi-parihar1@users.noreply.github.com>
2024-04-18 10:27:22 +0530

    18-04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Components/Pages/HomePage';
+import ContactPage from './Components/Pages/ContactPage';
 import './App.css';
 import React, { useState } from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import HomePage from './Components/Pages/HomePage';
-
+import ServicesPage from './Components/Pages/ServicesPage';
 function App() {
   return (
-    <>
-      <HomePage/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} active />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/services' element={<ServicesPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Pages/ServicesPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Header'
+
+export default function ServicesPage() {
+  return (
+    <div>
+        <Header/>
+      services
+    </div>
+  )
+}

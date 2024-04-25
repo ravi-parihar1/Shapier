fea0ef8aba1f2cdda8f9c7aa4091a6bb7d54b35c
ravi-parihar1 <91816794+ravi-parihar1@users.noreply.github.com>
2024-04-25 22:05:34 +0530

    build today

--- src/Components/Pages/Store.js
+++ src/Components/Pages/Store.js
@@ -5,6 +5,7 @@ import '../../css/store.css';
 
 
 
+
 export default function Store() {
     const [activeIndex, setActiveIndex] = useState(0);
 
@@ -47,47 +48,43 @@ export default function Store() {
             <div className="container-fluid">
                 <div className="row px-xl-5">
                     <div className="col-lg-8">
-                        <div id="my-carousel" className="carousel slide carousel-fade mb-30 mb-lg-0" data-ride="carousel">
-                            <ol className="carousel-indicators custom-indicators">
-                                {carouselItems.map((item, index) => (
-                                    <li key={index} onClick={() => handleSlideChange(index)} className={index === activeIndex ? 'active' : ''}></li>
-                                ))}
-                            </ol>
-                            <div className="carousel-inner">
+                        
+                            
+                            <div className="store-carousel-inner">
                                 {carouselItems.map((item, index) => (
                                     <div key={index} className={`carousel-item position-relative ${index === activeIndex ? 'active' : ''}`} style={{ height: '430px' }}>
                                         <img className="carousel-image" src={item.image} alt={`Slide ${index + 1}`} style={{ objectFit: 'cover' }} />
                                         <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                             <div className="carousel-content p-3" style={{ maxWidth: '700px' }}>
-                                                <h1 className="carousel-title text-white mb-3 animate__animated animate__bounceInDown">{item.name}</h1>
-                                                <p className="carousel-description mx-md-5 px-5 animate__animated animate__fadeInUp">{item.description}</p>
-                                                <p className="carousel-price text-white mb-0">{item.price}</p>
+                                                <h1 className="store-carousel-title text-white mb-3 animate__animated animate__bounceInDown">{item.name}</h1>
+                                                <p className="store-carousel-description mx-md-5 px-5 animate__animated animate__fadeInUp">{item.description}</p>
+                                                <p className="store-carousel-price text-white mb-0">{item.price}</p>
                                                 <a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
                                             </div>
                                         </div>
                                     </div>
                                 ))}
                             </div>
-                        </div>
+                        
                     </div>
 
                     <div className="col-lg-4">
                         {/* Static Offer Section */}
-                        <div className="custom-product-offer mb-30" style={{ height: '200px' }}>
+                        <div className="custom-product-offer" style={{ height: '200px' }}>
                             <img className="offer-image img-fluid" src="https://images.unsplash.com/flagged/photo-1600002368144-444430d3f3ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Offer 1" />
                             <div className="custom-offer-text">
                                 <h6 className="offer-text-uppercase text-white">Save 20%</h6>
                                 <h3 className="offer-title text-white mb-3">Special Offer</h3>
-                                <a href="#" className="btn btn-outline-primary">Shop Now</a>
+                                <a href="#" className="btn btn-outline-warning">Shop Now</a>
                             </div>
                         </div>
 
-                        <div className="custom-product-offer mb-30" style={{ height: '200px' }}>
+                        <div className="custom-product-offer " style={{ height: '200px' }}>
                             <img className="offer-image img-fluid" src="https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2016/2/16/2/Orig-Nils-Finne_window-styles-contemporary-exterior.jpg.rend.hgtvcom.616.462.suffix/1455654082188.jpeg" alt="Offer 1" />
                             <div className="custom-offer-text">
                                 <h6 className="offer-text-uppercase text-white">Save 20%</h6>
                                 <h3 className="offer-title text-white mb-3">Special Offer</h3>
-                                <a href="#" className="btn btn-primary">Shop Now</a>
+                                <a href="#" className="btn btn-outline-warning">Shop Now</a>
                             </div>
                         </div>
 

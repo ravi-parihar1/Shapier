de4fa62fcf3f424e566793c90127dd216d65f98c
ravi-parihar1 <91816794+ravi-parihar1@users.noreply.github.com>
2024-05-08 16:27:00 +0530

    log

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -1,9 +1,9 @@
-import {React , useState} from 'react'
+import { React, useState } from 'react'
 import '../../css/LoginPage.css'
 import { CiUser } from "react-icons/ci";
 import { FcGoogle } from "react-icons/fc";
 import { BsFacebook } from "react-icons/bs";
-
+import logo from '../../assets/HomePageAssets/Shapier-Logo-Final-Design Files-08062022/shapier_logo_final_svg.svg'
 
 
 export default function LoginPage() {
@@ -16,94 +16,107 @@ export default function LoginPage() {
   return (
     <>
       <div className={`login-container ${isSignUp ? "sign-up-mode" : ""}`}>
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-            <i className="fas fa-lock"></i>
-              {/* <CiUser /> */}
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" value="Login" className="login-btn1 solid" />
-            <p className="social-text">Or Sign in with social platforms</p>
-            <div className="social-media">
-              <a href="#" className="social-icon">
-              <BsFacebook />
-              </a>
+        
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+            <div className="shapier-name">
+                      <img src={logo} height={"70rem"} width={"70rem"} alt="" />
+                      <h2 className="title">Sign in</h2>
+                      {/* <h4 style={{ margin: "5%", fontWeight: "600" }}>SHAPIER</h4> */}
+                    </div>
               
-              <a href="#" className="social-icon">
-              <FcGoogle />
-              </a>
-              
-            </div>
-          </form>
-          <form action="#" className="sign-up-form">
-            <h2 className="title">Sign up</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Phone Number" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" className="login-btn1" value="Sign up" />
-            <p className="social-text">Or Sign up with social platforms</p>
-            <div className="social-media">
-              
-              <a href="#" className="social-icon">
-              <FcGoogle />
-              </a>
-              <a href="#" className="social-icon">
-              <BsFacebook />
-              </a>
-            </div>
-          </form>
-        </div>
-      </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                {/* <CiUser /> */}
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="login-btn1 solid" />
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <BsFacebook />
+                </a>
 
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content1">
-            <h3>New here ?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Debitis, ex ratione. Aliquid!
-            </p>
-            <button className="login-btn transparent" onClick={toggleMode}>
-              Sign up
-            </button>
+                <a href="#" className="social-icon">
+                  <FcGoogle />
+                </a>
+
+              </div>
+            </form>
+            <form action="#" className="sign-up-form">
+            <div className="shapier-name">
+                      <img src={logo} height={"50rem"} width={"50rem"} alt="" />
+                      <h2 className="title">Sign up</h2>
+                      {/* <h4 style={{ margin: "5%", fontWeight: "600" }}>SHAPIER</h4> */}
+                    </div>
+              
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Phone Number" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" className="login-btn1" value="Sign up" />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+
+                <a href="#" className="social-icon">
+                  <FcGoogle />
+                </a>
+                <a href="#" className="social-icon">
+                  <BsFacebook />
+                </a>
+              </div>
+            </form>
           </div>
-          <img src="img/log.svg" className="image" alt="" />
         </div>
-        <div className="panel right-panel">
-          <div className="content1">
-            <h3>One of us ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button className="login-btn transparent" onClick={toggleMode}>
-              Sign in
-            </button>
+
+        <div className="panels-container">
+          <div className="panel left-panel">
+          
+            <div className="content1">
+                    
+              <h3>New here ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Debitis, ex ratione. Aliquid!
+              </p>
+              <button className="login-btn transparent" onClick={toggleMode}>
+                Sign up
+              </button>
+            </div>
+            <img src="img/log.svg" className="image" alt="" />
           </div>
-          <img src="img/register.svg" className="image" alt="" />
+          <div className="panel right-panel">
+            <div className="content1">
+              <h3>One of us ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button className="login-btn transparent" onClick={toggleMode}>
+                Sign in
+              </button>
+            </div>
+            <img src="img/register.svg" className="image" alt="" />
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
